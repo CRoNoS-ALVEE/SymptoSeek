49387af3960de70cde2022ba0657296d9ba78ab6
Sabit Hasan Alvee <hasan.alvee15@gmail.com>
2025-02-28 20:59:32 +0600

    Edit

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -4,10 +4,9 @@ import { GoogleOAuthProvider } from "@react-oauth/google"
 import AuthContent from "./auth-content"
 
 export default function AuthPage() {
-  return (
-    <GoogleOAuthProvider clientId= "212676002216-mcn8d008etpp02tf7ujenk8l54isqo27.apps.googleusercontent.com" >
-      <AuthContent />
-    </GoogleOAuthProvider>
-  )
+    return (
+        <GoogleOAuthProvider clientId= "212676002216-mcn8d008etpp02tf7ujenk8l54isqo27.apps.googleusercontent.com" >
+            <AuthContent />
+        </GoogleOAuthProvider>
+    )
 }
-
